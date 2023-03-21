7757f04b164cdca768daeffc2a58e90f7aaef4f0
ayodiya <devayodiya@gmail.com>
2023-03-21 13:26:33 +0100

    change color name

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -3,7 +3,9 @@ import { createTheme } from '@mui/material/styles'
 export const theme = createTheme({
   palette: {
     primary: {
-      main: '#004CCC'
+      main: '#101828',
+      sec: '#004CCC',
+      tet: '#344054'
     }
   }
 })
