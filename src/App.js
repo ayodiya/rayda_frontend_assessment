602b5e55de55dbbb16ba91ac324942b3ace4cd56
ayodiya <devayodiya@gmail.com>
2023-03-21 10:26:03 +0100

    add react router dom

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import Box from '@mui/material/Box'
 import CssBaseline from '@mui/material/CssBaseline'
 import { ThemeProvider } from '@mui/material/styles'
+import { Route, Routes, Navigate } from 'react-router-dom'
 import "@fontsource/ibm-plex-sans";
 
+import Navbar from './components/Navbar';
 import { theme } from './utils/theme'
 import './styles/app.css'
+
 
 function App() {
   return (
     <>
       <CssBaseline />
       <ThemeProvider theme={theme}>
-        <Box>
-          Setup
-        </Box>
+        <Navbar/>
+        {/* <Routes>
+              <Route exact path='/' element={<AdminHome />} />
+            </Routes> */}
        </ThemeProvider>
     </>
   );
