173b19f03b349f95af3102fda24e44ab5dd24ca0
ayodiya <devayodiya@gmail.com>
2023-03-21 10:11:38 +0100

    setup mui theme

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import Box from '@mui/material/Box'
+import CssBaseline from '@mui/material/CssBaseline'
+import { ThemeProvider } from '@mui/material/styles'
 
+import { theme } from './utils/theme'
 
 function App() {
   return (
-    <div>
-      hgost
-    </div>
+    <>
+      <CssBaseline />
+      <ThemeProvider theme={theme}>
+        <Box>
+          Setup
+        </Box>
+       </ThemeProvider>
+    </>
   );
 }
 
