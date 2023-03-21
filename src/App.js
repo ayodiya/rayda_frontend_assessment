4f27c0db25bd73b869d4ac4e79886267073e56c6
ayodiya <devayodiya@gmail.com>
2023-03-21 10:22:19 +0100

    add custom font

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Box from '@mui/material/Box'
 import CssBaseline from '@mui/material/CssBaseline'
 import { ThemeProvider } from '@mui/material/styles'
+import "@fontsource/ibm-plex-sans";
 
 import { theme } from './utils/theme'
+import './styles/app.css'
 
 function App() {
   return (
